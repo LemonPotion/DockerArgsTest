ca15e28abf11b863f579779a56125e409df4db65
LemonPotion <140953841+LemonPotion@users.noreply.github.com>
2025-01-16 13:37:40 +0200

    Add files via upload
    Initial commit;

---
+++ DockerArgsTest/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ConsoleApp1;
+
+internal abstract class Program
+{
+    internal static void Main(string[] args)
+    {
+        Console.WriteLine("Environment variable:" + Environment.GetEnvironmentVariable("CONNECTION_STRING"));
+        
+        Console.WriteLine("List of args:");
+        
+        foreach (var arg in args)
+        {
+            Console.WriteLine(arg);
+        }
+    }
+}
